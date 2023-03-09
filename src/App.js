bdae576d2933b84e7fc59cc6f43c5a99aa4f3cca
vipena1 <viniciuspena81@gmail.com.br>
2023-03-09 20:59:57 -0300

    Aula 03

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
+
+
+function App() {
+  return (
+  <BrowserRouter>
+    <Header />
+      <Routes></Routes>
+    <Footer />
+  </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/header/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {AreaHeader} from './styled'
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <div>
+      <AreaHeader>
+      <div className='container'>
+          <div className='logo'>
+              <p>LOGO</p>
+          </div>
+          
+        <nav>
+          <ul>
+            <li> <a href='/'> INDEX</a></li>
+            <li> <a href="/sobre"> SOBRE </a></li>
+            <li> <a href="/cep"> CEP </a></li>
+            
+            <li> <Link to ='/sobre'> SOBRE V2 </Link></li>
+            <li> <Link to ='/'> INDEX V2 </Link></li>
+            <li> <Link to ='/cep'> CEP V2 </Link></li>
+          </ul>
+        </nav>
+      </div>
+     
+      </AreaHeader>
+    </div>
+  )
+};
+export default Header;
